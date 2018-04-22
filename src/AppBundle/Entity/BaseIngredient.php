db87473e09e5b13045f1b174a44a724a93f3550c
Linkinou <cedric.merouani@gmail.com>
2018-04-22 08:02:37 +0200

    Adding BaseIngredient

---
+++ src/AppBundle/Entity/BaseIngredient.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource
+ * @ORM\Entity
+ */
+class BaseIngredient
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return BaseIngredient
+     */
+    public function setName(string $name): BaseIngredient
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
